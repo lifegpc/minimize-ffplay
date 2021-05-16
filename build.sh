fc34e60724dd47aff96969d31f9123c60416e650
lifegpc <g1710431395@gmail.com>
2021-05-16 09:13:22 +0800

    upload build script

---
+++ build.sh
@@ -0,0 +1,3 @@
+cd ffmpeg || exit 1
+make || exit 1
+make install || exit 1
---
+++ configure.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+loc=`pwd`/ffbuild
+mkdir -p ffbuild
+cd ffmpeg
+./configure \
+	--disable-doc \
+	--disable-encoders \
+	--disable-ffmpeg \
+	--disable-ffprobe \
+	--disable-muxers \
+	--enable-gpl \
+	--enable-gmp \
+	--enable-gnutls \
+    # --enable-libass \
+    # --enable-libfontconfig \
+    # --enable-libfreetype \
+    # --enable-libfribidi \
+	--enable-small \
+	--enable-version3 \
+	'--extra-ldflags=-static' \
+	'--pkg-config-flags=--static' \
+	--prefix=$loc
+if [ $? -ne 0 ]
+then 
+	echo "::set-output name=conf::false"
+else
+	echo "::set-output name=conf::true"
+fi
