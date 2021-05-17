d7893d771e83ac9abd0f157d801faf1334abb713
lifegpc <g1710431395@gmail.com>
2021-05-17 22:37:24 +0800

    add self build fribidi

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ BROTLI=brotli.pkg.tar.zst
 HARFBUZZ=harfbuzz.pkg.tar.zst
 FONTCONFIG=fontconfig.pkg.tar.zst
 FREETYPE=freetype.pkg.tar.zst
+FRIBIDI=fribidi.pkg.tar.zst
 curl "https://github.com/lifegpc/static-mingw-w64-gnutls/releases/download/3.7.1-1/mingw-w64-x86_64-gnutls-3.7.1-1-any.pkg.tar.zst" -o $GNUTLS -L || exit 1
 pacman -U $GNUTLS $SETUPFLAGS || exit 1
 curl "https://github.com/lifegpc/static-mingw-w64-libxml2/releases/download/2.9.12-1/mingw-w64-x86_64-libxml2-2.9.12-1-any.pkg.tar.zst" -o $LIBXML2 -L || exit 1
@@ -17,3 +18,5 @@ curl "https://github.com/lifegpc/static-mingw-w64-fontconfig/releases/download/2
 pacman -U $FONTCONFIG $SETUPFLAGS || exit 1
 curl "https://github.com/lifegpc/static-mingw-w64-freetype/releases/download/2.10.4-2/mingw-w64-x86_64-freetype-2.10.4-2-any.pkg.tar.zst" -o $FREETYPE -L || exit 1
 pacman -U $FREETYPE $SETUPFLAGS || exit 1
+curl "https://github.com/lifegpc/static-mingw-w64-fribidi/releases/download/1.0.10-2/mingw-w64-x86_64-fribidi-1.0.10-2-any.pkg.tar.zst" -o $FRIBIDI -L || exit 1
+pacman -U $FRIBIDI $SETUPFLAGS || exit 1
