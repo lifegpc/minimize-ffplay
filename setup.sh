0b953684fb3f6d6fe6a876c32132242d6093c584
lifegpc <g1710431395@gmail.com>
2021-05-18 17:26:46 +0800

    use own libass build

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ HARFBUZZ=harfbuzz.pkg.tar.zst
 FONTCONFIG=fontconfig.pkg.tar.zst
 FREETYPE=freetype.pkg.tar.zst
 FRIBIDI=fribidi.pkg.tar.zst
+LIBASS=libass.pkg.tar.zst
 curl "https://github.com/lifegpc/static-mingw-w64-gnutls/releases/download/3.7.1-1/mingw-w64-x86_64-gnutls-3.7.1-1-any.pkg.tar.zst" -o $GNUTLS -L || exit 1
 pacman -U $GNUTLS $SETUPFLAGS || exit 1
 curl "https://github.com/lifegpc/static-mingw-w64-libxml2/releases/download/2.9.12-1/mingw-w64-x86_64-libxml2-2.9.12-1-any.pkg.tar.zst" -o $LIBXML2 -L || exit 1
@@ -20,3 +21,5 @@ curl "https://github.com/lifegpc/static-mingw-w64-freetype/releases/download/2.1
 pacman -U $FREETYPE $SETUPFLAGS || exit 1
 curl "https://github.com/lifegpc/static-mingw-w64-fribidi/releases/download/1.0.10-2/mingw-w64-x86_64-fribidi-1.0.10-2-any.pkg.tar.zst" -o $FRIBIDI -L || exit 1
 pacman -U $FRIBIDI $SETUPFLAGS || exit 1
+curl "https://github.com/lifegpc/static-mingw-w64-libass/releases/download/0.15.1-1/mingw-w64-x86_64-libass-0.15.1-1-any.pkg.tar.zst" -o $LIBASS -L || exit 1
+pacman -U $LIBASS $SETUPFLAGS || exit 1
