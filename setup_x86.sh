67a31295f9e7c004bc9b84b4944687bc5a980b49
lifegpc <g1710431395@gmail.com>
2021-05-29 09:14:23 +0800

    enable libxml2 for x86 build

--- setup_x86.sh
+++ setup_x86.sh
@@ -9,8 +9,8 @@ FRIBIDI=fribidi.pkg.tar.zst
 LIBASS=libass.pkg.tar.zst
 curl "https://github.com/lifegpc/static-mingw-w64-gnutls/releases/download/3.7.1-1/mingw-w64-i686-gnutls-3.7.1-1-any.pkg.tar.zst" -o $GNUTLS -L || exit 1
 pacman -U $GNUTLS $SETUPFLAGS || exit 1
-# curl "https://github.com/lifegpc/static-mingw-w64-libxml2/releases/download/2.9.12-1/mingw-w64-x86_64-libxml2-2.9.12-1-any.pkg.tar.zst" -o $LIBXML2 -L || exit 1
-# pacman -U $LIBXML2 $SETUPFLAGS || exit 1
+curl "https://github.com/lifegpc/static-mingw-w64-libxml2/releases/download/2.9.12-1/mingw-w64-x86_64-libxml2-2.9.12-1-any.pkg.tar.zst" -o $LIBXML2 -L || exit 1
+pacman -U $LIBXML2 $SETUPFLAGS || exit 1
 # curl "https://github.com/lifegpc/static-mingw-w64-brotli/releases/download/1.0.9-2/mingw-w64-x86_64-brotli-1.0.9-2-any.pkg.tar.zst" -o $BROTLI -L || exit 1
 # pacman -U $BROTLI $SETUPFLAGS || exit 1
 # curl "https://github.com/lifegpc/static-mingw-w64-harfbuzz/releases/download/2.8.1-1/mingw-w64-x86_64-harfbuzz-2.8.1-1-any.pkg.tar.zst" -o $HARFBUZZ -L || exit 1
